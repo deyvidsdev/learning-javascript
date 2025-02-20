7542636652403762b6ca0e13324fe36b7dc96b05
Deyvids <deyvidsalvino@gmail.com>
2025-02-19 21:45:28 -0500

    Tipo de variables
    1. Var
    2. Let
    3. Const

---
+++ 01-variables/app.js
@@ -0,0 +1,47 @@
+var hola = "Hola mundo"
+let hello = "Hello world"
+
+// console.log(window)
+
+/*
+    Var: Es una variable global
+    Ejemplo de bloque con var
+
+*/
+
+var musica = "Rock"
+console.log("Variable música antes del bloque", musica)
+
+// Esto es un bloque {}
+{
+    var musica = "Pop"
+    console.log("Variable música dentro del bloque", musica)
+}
+
+// El resultado es Pop porque var no respeta el bloque
+console.log("Variable música despues del bloque", musica)
+
+/*
+    Let: Es una variable local o en bloque
+    Ejemplo de bloque con let
+
+*/
+
+let color = "Azul"
+
+console.log("Variable color antes del bloque", color)
+
+{
+    let color = "Blanco"
+    console.log("Variable color dentro del bloque", color)
+}
+
+// El resultado es Azul porque let respeta el bloque
+console.log("Variable color despues del bloque", color)
+
+/*
+    Constantes: Con const no se puede reasignar el valor de la variable
+*/
+
+const PI = 3.1416
+console.log(PI)
