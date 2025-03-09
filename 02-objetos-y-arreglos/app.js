63ae1c37db823c40f36ff298c14c3910f56f7fc6
Deyvids <deyvidsalvino@gmail.com>
2025-03-08 20:51:04 -0500

    Documentacion de arreglos y objetos

---
+++ 02-objetos-y-arreglos/app.js
@@ -0,0 +1,26 @@
+/*
+
+    Objetos: Un objeto es un conjunto de datos y/o funcionalidades agrupadas bajo un nombre concreto.
+
+    Arreglos: Un arreglo es un conjunto de datos agrupados bajo un mismo nombre.
+
+*/
+
+
+// Un objeto
+const persona = {
+    nombre: "Deyvids",
+    edad: 20
+}
+
+// Arreglos
+const colores = ["Blanco", "Negro", "Azul"]
+
+console.log(persona)
+console.log(colores)
+
+persona.correo = "deyvidsalvino@gmail.com"
+colores.push("Rojo")
+
+console.log(persona)
+console.log(colores)
